ae513fcfd7fd1dff5366236182a368e3ca77fe69
Atokpassuzan <51885745+Atokpassuzan@users.noreply.github.com>
2020-04-19 20:04:18 +0545

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://Sujan:379379@project-wiqmt.mongodb.net/test?retryWrites=true&w=majority'
+    MongoURI: 'mongodb+srv://<username>:<password>@project-wiqmt.mongodb.net/test?retryWrites=true&w=majority'
 }
